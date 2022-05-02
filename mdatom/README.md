fde9ac92122cd79ce0a679f7d622a06bb2dd68dc
youwuyou <youwuyou@student.ethz.ch>
2022-05-02 15:57:09 +0200

    added the repo from the MD program

---
+++ mdatom/README.md
@@ -0,0 +1,4 @@
+# mdatom
+
+This repository contains the code for mdatom as well as the mdatom script (user manual)
+
---
+++ mdatom/lib/ParameterValidityChecker.h
@@ -0,0 +1,26 @@
+#ifndef PARAMETERVALIDITYCHECKER_H
+#define PARAMETERVALIDITYCHECKER_H
+
+#include "MDParameters.h"
+#include <string>
+
+/*!
+ * This class verifies the validity of the struct containing the MD parameters.
+ */
+class ParameterValidityChecker {
+public:
+    explicit ParameterValidityChecker(const MDParameters &parameters);
+
+    bool valid() const;
+
+    std::string getErrorMessage() const;
+
+private:
+    bool checkValidity();
+
+    const MDParameters &par;
+    bool parametersValid;
+    std::string errorMessage;
+};
+
+#endif // PARAMETERVALIDITYCHECKER_H
